23047e7a9a4a0923f758adaa20c7988c62bca931
arrijalulkhairi <khairi_21@mhs.unsyiah.ac.id>
2023-11-29 00:21:23 +0700

    feat: renew sql

--- sql/sekolah_db.sql
+++ sql/sekolah_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Nov 2023 pada 16.34
+-- Waktu pembuatan: 28 Nov 2023 pada 18.17
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -38,7 +38,33 @@ CREATE TABLE `absensi` (
 --
 
 INSERT INTO `absensi` (`nisn`, `tanggal`, `status`) VALUES
-(1234567890, '2023-11-22', 'Hadir');
+(1234567890, '2023-11-22', 'Hadir'),
+(1234567890, '2023-11-23', 'Hadir'),
+(1234567890, '2023-11-28', 'Hadir');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_pelajaran`
+--
+
+CREATE TABLE `data_pelajaran` (
+  `nisn` int(11) NOT NULL,
+  `pelajaran` varchar(50) NOT NULL,
+  `hari` varchar(10) NOT NULL,
+  `jam` time NOT NULL,
+  `guru` varchar(50) NOT NULL,
+  `nilai` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_pelajaran`
+--
+
+INSERT INTO `data_pelajaran` (`nisn`, `pelajaran`, `hari`, `jam`, `guru`, `nilai`) VALUES
+(1234567890, 'IPA', 'Senin', '10:00:00', 'Muhammad Ibnu', NULL),
+(1234567890, 'Matematika', 'Senin', '08:00:00', 'Adam Optimizer', NULL),
+(1234567890, 'IPS', 'Selasa', '08:00:00', 'Muhammad Sumbul', NULL);
 
 -- --------------------------------------------------------
 
@@ -50,15 +76,22 @@ CREATE TABLE `data_siswa` (
   `nisn` int(10) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `foto` varchar(255) DEFAULT NULL,
-  `nama_lengkap` varchar(100) DEFAULT NULL
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `nik` varchar(16) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(13) DEFAULT NULL,
+  `kelamin` enum('Laki-laki','Perempuan') DEFAULT NULL,
+  `tempat_lahir` varchar(50) DEFAULT NULL,
+  `agama` varchar(20) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `data_siswa`
 --
 
-INSERT INTO `data_siswa` (`nisn`, `tanggal_lahir`, `foto`, `nama_lengkap`) VALUES
-(1234567890, '2002-12-14', NULL, 'Arrijalul Khairi');
+INSERT INTO `data_siswa` (`nisn`, `tanggal_lahir`, `foto`, `nama_lengkap`, `nik`, `email`, `no_hp`, `kelamin`, `tempat_lahir`, `agama`, `alamat`) VALUES
+(1234567890, '2002-12-14', NULL, 'Arrijalul Khairi', '1234561412020001', 'ijall@gmail.com', '0895600006146', 'Laki-laki', 'Langsa', 'Islam', 'BTN BSP BLOK C NO. 27 C, Matang Seulimeng, Langsa Barat, Kota Langsa, Aceh, 24413');
 
 --
 -- Indexes for dumped tables
@@ -69,6 +102,12 @@ INSERT INTO `data_siswa` (`nisn`, `tanggal_lahir`, `foto`, `nama_lengkap`) VALUE
 --
 ALTER TABLE `absensi`
   ADD KEY `fk_nisn` (`nisn`);
+
+--
+-- Indeks untuk tabel `data_pelajaran`
+--
+ALTER TABLE `data_pelajaran`
+  ADD KEY `fk_nisn_pelajaran` (`nisn`);
 
 --
 -- Indeks untuk tabel `data_siswa`
@@ -85,6 +124,12 @@ ALTER TABLE `data_siswa`
 --
 ALTER TABLE `absensi`
   ADD CONSTRAINT `fk_nisn` FOREIGN KEY (`nisn`) REFERENCES `data_siswa` (`nisn`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `data_pelajaran`
+--
+ALTER TABLE `data_pelajaran`
+  ADD CONSTRAINT `fk_nisn_pelajaran` FOREIGN KEY (`nisn`) REFERENCES `data_siswa` (`nisn`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
